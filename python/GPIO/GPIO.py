9dd55ce0afa5f3601071a0440d98bd858eb7c6d6
Rui Paulo <rpaulo@felyko.com>
2014-02-19 16:39:10 -0800

    python: add errno parsing, a __del__ method, a close method and explain the errors.

--- python/GPIO/GPIO.py
+++ python/GPIO/GPIO.py
@@ -24,6 +24,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+import os
 from ctypes import *
 
 class GPIOConfig(Structure):
@@ -34,15 +35,24 @@ class GPIOConfig(Structure):
 
 class GPIO:
     def __init__(self, dev=0):
-        self.lib = CDLL('libgpio.so')
+        self.lib = CDLL('libgpio.so', use_errno=True)
         self.handle = self.lib.gpio_open(dev)
         if self.handle == -1:
-            raise IOError
+            raise OSError(os.strerror(get_errno()))
+
+    def __del__(self):
+        if self.handle != -1:
+            self.lib.gpio_close(handle)
+        del self.lib
+
+    def close(self):
+        if self.handle != -1:
+            self.lib.gpio_close(self.handle)
 
     def pin_list(self):
         gcp = POINTER(GPIOConfig)
         pins = self.lib.gpio_pin_list(self.handle, byref(gcp))
-        if pins > 0:
+        if pins >= 0:
             da = []
             for i in range(0, pins):
                 d = { 'pin': gcp[i].pin,
@@ -52,7 +62,7 @@ class GPIO:
                 da.append(d)
             return da
         else:
-            return OSError
+            return OSError(os.strerror(get_errno()))
 
     def pin_config(self, pin):
         gc = GPIOConfig()
@@ -60,73 +70,73 @@ class GPIO:
         if self.lib.gpio_pin_config(self.handle, byref(gc)) == 0:
             return gc.config
         else:
-            raise OSError
+            raise OSError('Unable to retrieve pin configuration')
    
     def pin_set_flags(self, pin, flags):
         gc = GPIOConfig()
         gc.pin = pin
         gc.flags = flags
         if self.lib.gpio_pin_set_flags(self.handle, byref(gc)) != 0:
-            raise OSError
+            raise OSError('Unable to set pin flags')
 
     def pin_get(self, pin):
         return self.lib.gpio_pin_get(self.handle, pin)
 
     def pin_set(self, pin, value):
         if self.lib.gpio_pin_set(self.handle, pin, value) != 0:
-            raise OSError
+            raise OSError('Unable to set pin value')
 
     def pin_toggle(self, pin):
         if self.lib.gpio_pin_toggle(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to toggle pin')
 
     def pin_low(self, pin):
         if self.lib.gpio_pin_low(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to low')
 
     def pin_high(self, pin):
         if self.lib.gpio_pin_high(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to high')
 
     def pin_input(self, pin):
         if self.lib.gpio_pin_input(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to input')
 
     def pin_output(self, pin):
         if self.lib.gpio_pin_output(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to output')
 
     def pin_opendrain(self, pin):
         if self.lib.gpio_pin_opendrain(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to open-drain')
 
     def pin_pushpull(self, pin):
         if self.lib.gpio_pin_pushpull(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to push-pull')
 
     def pin_tristate(self, pin):
         if self.lib.gpio_pin_tristate(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to tristate')
 
     def pin_pullup(self, pin):
         if self.lib.gpio_pin_pullup(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to pullup')
 
     def pin_pulldown(self, pin):
         if self.lib.gpio_pin_pulldown(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to pulldown')
 
     def pin_invin(self, pin):
         if self.lib.gpio_pin_invin(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to inv-in')
 
     def pin_invout(self, pin):
         if self.lib.gpio_pin_invout(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to inv-out')
 
     def pin_pulsate(self, pin):
         if self.lib.gpio_pin_pulsate(self.handle, pin) != 0:
-            raise OSError
+            raise OSError('Unable to set pin to pulsate')
 
     # Pin definitions from /usr/include/sys/gpio.h
     pin_low = 0x00
